85aee312b821b45b4b9b31d01ab3426077c3070c
Harry Weaver <harryweaver7@gmail.com>
2025-11-02 15:46:12 +0000

    Game View added

--- TicTacToeCourse/TicTacToeCourse/Core/GameView.swift
+++ TicTacToeCourse/TicTacToeCourse/Core/GameView.swift
@@ -12,10 +12,104 @@ struct GameView: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            playerStatsView
+            Spacer()
+            gameBoardView
+            Spacer()
+            actionButtonsView
+        }
+        .infinityFrame()
+        .padding()
+        .background(Color.appTheme.viewBackground)
+        .sheet(isPresented: Binding(
+            get: { viewModel.showInnerSheet },
+            set: { _ in }
+        )) {
+            GameResultView(gameState: viewModel.gameState, resetGame: viewModel.resetGame)
+        }
+    }
+}
+
+private extension GameView {
+    var playerStatsView: some View {
+        HStack(spacing: 0)
+        {
+            PlayerInGameView(
+                player: viewModel.player1.profile,
+                orientation: .left,
+                isCurrentPlayer: viewModel.currentPlayer.cellSymbol == .x,
+                winsCount: viewModel.player1.wins
+            )
+            Spacer(minLength: 5)
+            PlayerInGameView(
+                player: viewModel.player2.profile,
+                orientation: .right,
+                isCurrentPlayer: viewModel.currentPlayer.cellSymbol == .o,
+                winsCount: viewModel.player2.wins
+            )
+        }
+        .animation(.easeInOut(duration: GameConstants.playerSwapAnimationDuration), value:
+                    viewModel.currentPlayer.cellSymbol)
+        .transition(.opacity.combined(with: .scale))
+    }
+    
+    var gameBoardView: some View {
+        GameBoardView(
+            board: viewModel.board,
+            winningCells: viewModel.winningCells,
+            onCellTap: viewModel.playHumanMove
+        )
+        .disabled(viewModel.isplayHumanMoveDisabled)
+    }
+    
+    var actionButtonsView: some View {
+        HStack(spacing: 32) {
+            //placeholder for additional functionality
+            actionButtonView(sfsymbol: "house")
+                .opacity(0)
+            actionButtonView(sfsymbol: "arrow.clockwise", buttonSize: .large)
+                .button(.press) {
+                    viewModel.resetGame()
+                }
+            actionButtonView(sfsymbol: "house")
+                .button(.press) {
+                    viewModel.goSetupmode()
+                }
+          
+            }
+        }
+    
+
+    func actionButtonView(sfsymbol: String, buttonSize: ButtonSize = .medium) -> some View {
+        Image(systemName: sfsymbol)
+            .resizable()
+            .scaledToFit()
+            .padding(12)
+            .foregroundColor(Color.appTheme.text.opacity(0.6))
+            .frame(width: buttonSize.size, height: buttonSize.size)
+            .background(Color.appTheme.cellBackground)
+            .clipShape(Circle())
+            .shadow(.regular)
     }
 }
 
 #Preview {
     GameView()
+}
+
+extension GameView {
+    enum ButtonSize {
+        case medium
+        case large
+    }
+}
+
+extension GameView.ButtonSize {
+    var size: CGFloat {
+        switch self {
+        case .medium: 45
+        case .large: 60
+        }
+    }
 }
